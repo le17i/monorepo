1dc62e3c25f2e0869de60aa5273d53922b0ee483
Leandro Dias <leandro.dias@cargox.com.br>
2020-04-29 16:51:31 -0300

    fix(MrComponents): rename file and fix lint errors

--- packages/MrComponents/src/main.js
+++
@@ -1,9 +0,0 @@
-import MrBtn from './components/MrBtn';
-import MrHeader from './components/MrHeader';
-import MrInput from './components/MrInput';
-
-export default {
-  MrBtn,
-  MrHeader,
-  MrInput
-}
