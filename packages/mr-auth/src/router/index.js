41114d421b0ac44eb7b24697881b89cbc28343fe
Leandro Dias <leandro.dias@cargox.com.br>
2020-04-29 12:19:42 -0300

    refactor(MrAuth): exports routes list

--- packages/mr-auth/src/router/index.js
+++ packages/mr-auth/src/router/index.js
@@ -1,29 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Recover from '../views/Recover.vue'
+import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+export const AUTH_ROUTES = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/recover',
+    name: 'Recover',
+    component: Recover
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: AUTH_ROUTES
 })
 
 export default router
