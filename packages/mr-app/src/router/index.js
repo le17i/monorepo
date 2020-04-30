7d0d893b1b8c2b1c69d06735ef64e37efff5400f
Leandro Dias <leandro.dias@cargox.com.br>
2020-04-30 10:52:59 -0300

    feat(MrAuth): integrates with mr-auth module

--- packages/mr-app/src/router/index.js
+++ packages/mr-app/src/router/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import { AUTH_ROUTES } from '@le17i/mr-auth/src/router'
+import { AUTH_ROUTES } from '@le17i/mr-auth'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
-
-console.log('@le17i/mr-auth', AUTH_ROUTES)
 
 const routes = [
   {
@@ -20,7 +18,8 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  ...AUTH_ROUTES
 ]
 
 const router = new VueRouter({
